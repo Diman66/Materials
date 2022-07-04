d78cd710c7194a81e7e697764c09c1709045732d
Diman66 <93481846+Diman66@users.noreply.github.com>
2022-07-04 23:15:06 +0500

    Add files via upload

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,4 +1,5 @@
-<footer class="footer py-4 mt-5 bg-light">
+    </div>
+    <footer class="footer py-4 mt-5 bg-light">
         <div class="container">
             <div class="row">
                 <div class="col text-muted">Test</div>
@@ -7,7 +8,20 @@
     </footer>
 </div>
 <!-- Optional JavaScript; choose one of the two! -->
-
+<script>
+    let btnDelete = document.querySelectorAll('.btnDelete');
+    for (let elem of btnDelete) {
+        elem.onclick = function(e) {
+            let isDelete = confirm("Удалить материал?");
+            if (isDelete) {
+                return true;
+            } else {
+                return false;
+            };  
+        };
+    };  
+    
+</script>
 <!-- Option 1: Bootstrap Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
--- views/layout/header.php
+++ views/layout/header.php
@@ -16,21 +16,22 @@
     <div class="content">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="#">Test</a>
+                <a class="navbar-brand" href="/">Test</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
+                    <?php $url = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)); ?>
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/materials">Материалы</a>
+                            <a class="nav-link <?php if ($url[1] == "materials") {echo "active";}?>" aria-current="page" href="/materials">Материалы</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/tags">Теги</a>
+                            <a class="nav-link <?php if ($url[1] == "tags") {echo "active";}?>" href="/tags">Теги</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/categories">Категории</a>
+                            <a class="nav-link <?php if ($url[1] == "categories") {echo "active";}?>" href="/categories">Категории</a>
                         </li>
                     </ul>
                 </div>
